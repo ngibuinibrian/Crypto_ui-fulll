77f5517d1a489de92bb3cfff393fac3229d7e92f
ngibuinibrian <ngibuini7@gmail.com>
2025-06-23 12:50:11 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LandingPage from './components/LandingPage';
+import AddProjectPage from './components/AddProjectPage';
+
+export default function App() {
+  return (
+    <div>
+      <LandingPage />
+      <AddProjectPage />
+    </div>
+  );
+}
