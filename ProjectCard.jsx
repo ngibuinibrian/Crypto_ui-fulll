ed6cfbca4796675cd643c7a8dbda0a1b6c333756
ngibuinibrian <ngibuini7@gmail.com>
2025-06-23 21:32:04 +0300

    Add files via upload

---
+++ ProjectCard.jsx
@@ -0,0 +1,6 @@
+// components/ProjectCard.jsx
+function ProjectCard() {
+  return <div style={{border: "1px solid #ccc", padding: "10px"}}>✅ Project Card Component Rendered</div>;
+}
+
+export default ProjectCard;
