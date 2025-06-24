5b16bf949d80974b701caed910af3759ebfcad32
ngibuinibrian <ngibuini7@gmail.com>
2025-06-24 10:48:17 +0300

    Delete ProjectCard.jsx

--- ProjectCard.jsx
+++
@@ -1,6 +0,0 @@
-// components/ProjectCard.jsx
-function ProjectCard() {
-  return <div style={{border: "1px solid #ccc", padding: "10px"}}>✅ Project Card Component Rendered</div>;
-}
-
-export default ProjectCard;
